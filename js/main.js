60e743604e0329cb21c37104ace6018e839402de
tontake <graceglory591@gmail.com>
2019-03-30 15:09:19 -0700

    updated

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 10) { 
+            console.log($(this).scrollTop());
+			$("#scroll").css("display","block");
+        } else { 
+            
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 10 }, 600); 
+        return false; 
+    }); 
+	 
+});
